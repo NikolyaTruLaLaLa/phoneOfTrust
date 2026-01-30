0c6e32e6716d67e81fa1306cc91f570cc0fb7235
NikolyaTruLaLaLa <1126ignatenkonikolay@gmail.com>
2026-01-30 12:46:03 +0300

    Adding 2 validations for chat model: couldn't change 'ended' chat status and chats with status not ended are unique. Also added allowed statuses in model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,8 +1,18 @@
 class Chat < ApplicationRecord
+  AVAILABLE_STATUSES = %w[waiting active ended].freeze
+
   has_many :messages, dependent: :destroy
 
-
   validates :visitors_token, uniqueness: true, presence: true
-  validates :status, presence: true
-  validates :visitors_init_token, presence: true
+  validates :status, presence: true, inclusion: { in: AVAILABLE_STATUSES }
+  validates :visitors_init_token, presence: true, uniqueness: { 
+                                                                conditions: -> { where.not(status: 'ended') }
+                                                              }
+  validate :protect_closed_status_change, on: :update
+
+def protect_closed_status_change
+  if status_was == 'ended' && status_changed?
+    errors.add(:status, "закрытые записи нельзя изменять")
+  end
+end
 end
